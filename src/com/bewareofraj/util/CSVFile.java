f7b5119744fc17683f2fd6aed1448e565d7d6f65
Rajesh Ramsaroop <raj@bewareofraj.com>
2014-05-10 10:11:56 -0400

    create CSV file parser

---
+++ src/com/bewareofraj/util/CSVFile.java
@@ -0,0 +1,128 @@
+/**
+ * This class is responsible for opening and parsing a CSV File.
+ */
+package com.bewareofraj.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ * @author Raj
+ * @version 1.0
+ */
+/**
+ * @author Raj
+ *
+ */
+public class CSVFile {
+	
+	private File sourceFile;
+	private String separator;
+	private ArrayList<String[]> records;
+	
+	/**
+	 * Default constructor. Sets the default separator to a comma
+	 */
+	public CSVFile() {
+		// Use comma as default separator
+		this.separator = ",";
+	}
+	
+	/**
+	 * Constructor that takes a file argument.
+	 * @param _file The CSV file
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public CSVFile(File _file) throws FileNotFoundException, IOException {
+		this();
+		this.sourceFile = _file;
+		open();
+	}
+
+	/**
+	 * @return the sourceFile
+	 */
+	public File getSourceFile() {
+		return sourceFile;
+	}
+
+	/**
+	 * @param _sourceFile the sourceFile to set
+	 */
+	public void setSourceFile(File _sourceFile) {
+		this.sourceFile = _sourceFile;
+	}
+
+	/**
+	 * @return the separator
+	 */
+	public String getSeparator() {
+		return separator;
+	}
+
+	/**
+	 * @param _separator the separator to set
+	 */
+	public void setSeparator(String _separator) {
+		this.separator = _separator;
+	}
+
+	/**
+	 * @return the records
+	 */
+	public ArrayList<String[]> getRecords() {
+		return records;
+	}
+
+	/**
+	 * @param _records the records to set
+	 */
+	public void setRecords(ArrayList<String[]> _records) {
+		this.records = _records;
+	}
+	
+	/**
+	 * Open the file on disk and set the records ArrayList.
+	 * Make sure sourceFile is set.
+	 * @throws IOException, FileNotFoundException 
+	 */
+	private void open() throws IOException, FileNotFoundException {
+		BufferedReader reader = new BufferedReader(new FileReader(this.sourceFile));
+		
+		String line;
+		while ( (line = reader.readLine()) != null) {
+			String[] record = line.split(this.separator);
+			this.records.add(record);
+		}
+		
+		// close resource
+		reader.close();
+	}
+	
+	/**
+	 * This method writes the records to the specified file. Overwrites if file already exists.
+	 * @param _file
+	 * @throws FileNotFoundException 
+	 */
+	public void saveFile(File _file) throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(_file);
+		
+		for (String[] record : this.records) {
+			for (String r : record) {
+				writer.print(r);
+				writer.print(this.separator);
+			}
+			writer.println("");
+		}
+		
+		// close resource
+		writer.close();
+	}
+
+}
